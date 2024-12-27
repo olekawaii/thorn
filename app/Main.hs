9b1e1e5c03ec76c27a13b56d29e9139e477462fd
Oleksiy Buell <olekawaii@proton.me>
2024-12-26 20:08:12 -0500

    cleaned up a bit

--- app/Main.hs
+++ app/Main.hs
@@ -2,54 +2,46 @@
 
 module Main (main) where
 
-import Lib
+-- import Lib
 
 import Control.Monad
 import Control.Arrow
 import Text.Read
 import Data.Maybe
 import Data.Bifunctor
-import System.IO
+-- import System.IO
 import Data.Tuple
 
 import Types
 
 
-main 
-  = interact 
-  $ fromEither 
-  . bimap show (show . map (\(x,(_,list)) -> (x, getDependencies $ map snd list))) 
-  . (cutSpace >=> parse)
+main = interact $ fromEither . bimap show (show . map (name *** getDependencies . map snd)) . (cutSpace >=> parse)
 
 fromEither :: Either a a -> a
 fromEither (Left  a) = a
 fromEither (Right a) = a
--- main = readFile "image.aa" >>= putStr . show . head . parse . cutSpace "image.aa" 
 
 cons = (:)
-app :: [a] -> a -> [a]
-app  x y = x <> [y]
+append :: a -> [a] -> [a]
+append  x y = y <> [x]
 
-parse :: RawData -> Either Error (Map Name Body)   
+parse :: RawData -> Either Error (Map Header RawData)   
 parse []           = return []
-parse ((l,x) : xs) = parseHeader l x >>= \a -> case (head . words . snd) <$> listToMaybe xs of
-  Nothing      -> Left $ Custom "Header does not have a body" l
-  Just "frame" -> scriptList >>= \script -> (cons (name a, (a, script)) <$> parse (drop (length script + 1) xs))
-    where 
-      scriptList = fmap fst $ getDelimiter xs l "script" "end" []
-  -- Just "frame" -> Left $ Custom "I found the beginning of a script" l
-  Just _       -> cons (name a, (a, take (height a * frames a) xs)) <$> parse (drop (height a * frames a) xs)
+parse ((l,x) : xs) = parseHeader l x >>= \header -> case head . words . snd <$> listToMaybe xs of
+  Nothing      -> Left $ Custom "Header is lacking a body" l
+  Just "frame" -> getDelimiter xs l "script" "end" [] >>= \(script, other) -> cons (header, script) <$> parse other
+  Just _       -> cons (header, take len xs) <$> parse (drop len xs)
+    where len = height header * frames header
 
--- finds closing delimiter and returs upt to and after it
+-- finds closing delimiter and returs up to and after it
 -- getDelimiter :: RawData -> LineNumber -> original -> good -> [bad] -> Either Error (garbage, RawData)
 getDelimiter :: RawData -> LineNumber -> String -> String -> [String] -> Either Error (RawData, RawData)
 getDelimiter []           olden original _     _  = Left $ Delimiter original olden
 getDelimiter ((l,x) : xs) olden original good bad = case words x of
-  w | length w > 1 || w == [] -> bimap (<> [(l,x)]) id <$> getDelimiter xs olden original good bad
-    | elem (head w) bad       -> Left $ Delimiter original olden
-    | head w == good          -> return ([],xs)
-    | otherwise               -> bimap (<> [(l,x)]) id <$> getDelimiter xs olden original good bad   
-
+  [w] -> if 
+    | w == good  -> return ([],xs)
+    | elem w bad -> Left $ Delimiter original olden
+  _   -> bimap (append (l,x)) id <$> getDelimiter xs olden original good bad   
 
 cutSpace :: String -> Either Error RawData
 cutSpace = cleanGood . zip [1..] . lines
@@ -59,7 +51,7 @@ cutSpace = cleanGood . zip [1..] . lines
       []      -> cleanGood xs
       ["com"] -> getDelimiter xs l "com" "moc" ["com"] >>= cleanGood . snd
       ["moc"] -> Left $ Delimiter "moc" l
-      _       -> ((l,x) :) <$> cleanGood xs
+      _       -> cons (l,x) <$> cleanGood xs
     
 getDependencies :: [String] -> [Name]
 getDependencies 
@@ -74,7 +66,7 @@ getDependencies
 win :: EpicGifData -> Dependencies -> NamedRawData -> Either Error EpicGifData
 win uwu []          _ = pure uwu
 win uwu ((n,[]):xs) d = solve uwu n (d ! n) >>= \s -> win (s : uwu) (bimap id (filter (/= n)) <$> xs) d
-win uwu (x:xs)      d = win uwu (xs <> [x]) d
+win uwu (x:xs)      d = win uwu (append x xs) d
 
 (!) :: Eq a => Map a b -> a -> b
 (!) m a = snd . head $ filter ((== a) . fst) m
@@ -92,24 +84,24 @@ colorize h s = undefined
   $ lines s
 
 parseHeader :: LineNumber -> String -> Either Error Header
-parseHeader line s = case words s of 
-  c | length c /= 4 -> Left $ Parse "header" "4 values" (show $ length c) line
-    | isNothing width -> Left $ Parse "width"  "an int" (show $ c !! 0) line
-    | isNothing height -> Left $ Parse "height" "an int" (show $ c !! 1) line
-    | isNothing frames -> Left $ Parse "number of frames" "an int" (show $ c !! 2) line 
-    | any (`notElem` '_' : ['a'..'z']) name -> Left $ Parse "name" "only chars a-z and _" (show name) line
-    | otherwise -> Right Header {
-        width  = fromJust width, 
-        height = fromJust height,
-        frames = fromJust frames,
-        name   = name
-      }
-    where
-      width  = readMaybe (c !! 0)
-      height = readMaybe (c !! 1)
-      frames = readMaybe (c !! 2)
-      name   = c !! 3
-    
+parseHeader l = bimap ($ l) id . parseHelper . words
+  where 
+    parseHelper :: [String] -> Either (LineNumber -> Error) Header
+    parseHelper [a,b,c,d] = case readMaybe a of 
+      Nothing    -> Left $ Parse "width"  "an int" (show a) 
+      Just width -> case readMaybe b of 
+        Nothing     -> Left $ Parse "height" "an int" (show b)
+        Just height -> case readMaybe c of 
+          Nothing     -> Left $ Parse "number of frames" "an int" (show c) 
+          Just frames -> if
+            | any (`notElem` '_' : ['a'..'z']) d -> Left $ Parse "name" "only chars a-z and _" (show d)
+            | otherwise -> Right Header {
+                width  = width, 
+                height = height,
+                frames = frames,
+                name   = d
+              }
+    parseHelper w = Left $ Parse "header" "4 values" (show $ length w) 
 {-
  - tree structure
  - could be texture.uwu or script.uwuscript
