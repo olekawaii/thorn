de4619b3a694128a259aa1f8b1305040a2834f59
oleksiy <olekawaii@proton.me>
2025-06-24 14:39:10 -0400

    add scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 cd src
 ghc -O main.hs
-cp main /usr/local/bin/ascr
 cd ..
-cp uwu /usr/local/bin/uwu
-printf 'moved executables `ascr` and `uwu` to ~/.local/bin/\n'
---
+++ install.sh
@@ -0,0 +1,2 @@
+cp uwu /usr/local/bin/uwu
+cp src/main /usr/local/bin/ascr
