dc1094ef1c4517e4589c23f0f0590f9487f91d88
olekawaii <olekawaii@proton.me>
2025-10-07 15:48:15 -0400

    .

--- examples/mushroom_new/output/output.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $(tput cols) -lt 16 -o $(tput lines) -lt 7 ]; then
-    printf "\33[91mterminal is too small\nmust be at least 16 by 7 cells\33[0m\n" >&2
-    exit 1
-fi
-
-printf '  \33[91m__\33[97mc\33[91m__\33[97m,\33[91m___\n(_\33[97mc\33[91m_\33[97m,O\33[91m_/\134_\33[97m3\33[91m_)\n     \33[97m| |\n    /_. \134\n     | |    \33[91m__\n     \33[97m|"|  \33[91m(\33[97mc\33[91m_\33[97mo\33[91m_)\n     \33[97m| |   (..)\n'
